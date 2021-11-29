0483253d3948241f439869a163c2efad0961b12f
Ahmed Salih Cezayir <63608956+AhmedSalihCezayir@users.noreply.github.com>
2021-11-29 19:57:21 +0300

    bootstrap dependency

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import { store } from "./storage/store.js"
 import { router } from "./router/router.js"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const app = createApp(App);
 app.use(store);
