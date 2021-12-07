d220fb5d517bc5b6132fbd7958b5af7286f512a4
Ahmed Salih Cezayir <63608956+AhmedSalihCezayir@users.noreply.github.com>
2021-12-08 00:01:42 +0300

    Last commit for quasar configuration

---
+++ Frontend/src/quasar-user-options.js
@@ -0,0 +1,15 @@
+
+import './styles/quasar.sass'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  }
+}
