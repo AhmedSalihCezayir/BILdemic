5afad7dd1dae6beba2aa8743a62eab993f504d1a
Ezgi Lena Sönmez <lena.sonmez@ug.bilkent.edu.tr>
2021-12-22 17:12:48 +0300

    changes in realtime database :)

--- Frontend/src/classes/LoginManager.ts
+++ Frontend/src/classes/LoginManager.ts
@@ -7,23 +7,23 @@ import {
 
 import { getFirestore, collection, setDoc, addDoc, doc } from "firebase/firestore"; 
 import { useRouter } from 'vue-router';
+import {getDatabase, ref, set} from "firebase/database";
 import CafeteriaStaff from "./CafeteriaStaff";
 import DiagnovirTester from "./DiagnovirTester";
 import HealthCenterStaff from "./HealthCenterStaff";
 import Instructor from "./Instructor";
 import SportStaff from "./SportStaff";
-import Student from './Student'
+import Student from './Student';
     
 export default class LoginManager {
 
     //Properties
     private static instance: LoginManager | null = null;
-    public mAuth: Auth;
     //public CRUDManager crudManager;
 
     //Constructor
     private LoginManager() {
-        this.mAuth = getAuth();
+        
     }
 
     //Methods
@@ -42,41 +42,37 @@ export default class LoginManager {
             if (userCredential) {
                 await sendEmailVerification(userCredential.user);
             }
-            const db = getFirestore();
-            const crole = role + "s";
-            const doc1 = doc(db, crole, userCredential.user.uid);
-
-            
+            const db = getDatabase();
+            const crole = role.replace(/ /g, "") + "s";
 
             if (role === "Student") {
                 const student = new Student(name, mail, password, role, address, phoneNumber, hesCode, ID, resideInDorm, roomMateNames);
-                
-                await setDoc(doc1, Object.assign({}, student));
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), student);
                 return true;
             }
             else if (role === "Instructor") {
                 const instructor = new Instructor(name, mail, password, role, address, phoneNumber, hesCode, ID, false, null);
-                await setDoc(doc1, instructor);
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), instructor);
                 return true;
             }
-            else if (role === "CafeteriaStaff") {
+            else if (role === "Cafeteria Staff") {
                 const cafeteriaStaff = new CafeteriaStaff(name, mail, password, role, address, phoneNumber, hesCode);
-                await setDoc(doc1, cafeteriaStaff);
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), cafeteriaStaff);
                 return true;
             }
-            else if (role === "HealthCenterStaff") {
+            else if (role === "Health Center Staff") {
                 const healthCenterStaff = new HealthCenterStaff(name, mail, password, role, address, phoneNumber, hesCode);
-                await setDoc(doc1, healthCenterStaff);
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), healthCenterStaff);
                 return true;
             }
-            else if (role === "DiagnovirTester") {
+            else if (role === "Diagnovir Tester") {
                 const diagnovirTester = new DiagnovirTester(name, mail, password, role, address, phoneNumber, hesCode);
-                await setDoc(doc1, diagnovirTester);
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), diagnovirTester);
                 return true;
             }
-            else if (role === "SportStaff") {
+            else if (role === "Sports Center Staff") {
                 const sportStaff = new SportStaff(name, mail, password, role, address, phoneNumber, hesCode);
-                await setDoc(doc1, sportStaff);
+                set(ref(db, `Users/${crole}/${userCredential.user.uid}`), sportStaff);
                 return true;
             }
             else {
@@ -88,60 +84,7 @@ export default class LoginManager {
         return false;
     });
     return false;
-    }
-
-    /**public mAuth:FirebaseAuth;
-    //public CRUDManager crudManager;
-
-    //Constructor
-    private LoginManager() {
-        this.mAuth = FirebaseAuth.getInstance();
-    }
-
-    //Methods
-    public static LoginManager getInstance(){
-        if(instance == null) {
-            instance = new LoginManager();
-        }
-        return instance;
-    }
-
-    public static boolean createUser(String name, String mail, String password, String role, String address, String phoneNumber, String hesCode, int ID, boolean resideInDorm, String roomMateNames) throws InterruptedException, ExecutionException {
-        if (role.equals("Student")) {
-            Student student = new Student(name, mail, password, role, address, phoneNumber, hesCode, ID, resideInDorm, roomMateNames);
-            CRUDManager.getInstance().createCRUD(student);
-            return true;
-        }
-        else if (role.equals("Instructor")) {
-            Instructor instructor = new Instructor(name, mail, password, role, address, phoneNumber, hesCode, ID, false, null);
-            CRUDManager.getInstance().createCRUD(instructor);
-            return true;
-        }
-        else if (role.equals("CafeteriaStaff")) {
-            CafeteriaStaff cafeteriaStaff = new CafeteriaStaff(name, mail, password, role, address, phoneNumber, hesCode);
-            CRUDManager.getInstance().createCRUD(cafeteriaStaff);
-            return true;
-        }
-        else if (role.equals("HealthCenterStaff")) {
-            HealthCenterStaff healthCenterStaff = new HealthCenterStaff(name, mail, password, role, address, phoneNumber, hesCode);
-            CRUDManager.getInstance().createCRUD(healthCenterStaff);
-            return true;
-        }
-        else if (role.equals("DiagnovirTester")) {
-            DiagnovirTester diagnovirTester = new DiagnovirTester(name, mail, password, role, address, phoneNumber, hesCode);
-            CRUDManager.getInstance().createCRUD(diagnovirTester);
-            return true;
-        }
-        else if (role.equals("SportStaff")) {
-            SportStaff sportStaff = new SportStaff(name, mail, password, role, address, phoneNumber, hesCode);
-            CRUDManager.getInstance().createCRUD(sportStaff);
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    public static void sendCustomEmail(String email, String link) {}*/
+    } 
+    
 
 }
