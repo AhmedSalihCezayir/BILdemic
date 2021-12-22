a02cf7078cea13847e6d9fad376ef605eaf7fb17
Ahmed Salih Cezayir <63608956+AhmedSalihCezayir@users.noreply.github.com>
2021-12-22 03:20:34 +0300

    Backend Connection

--- Frontend/src/hooks/useAuth.js
+++ Frontend/src/hooks/useAuth.js
@@ -19,8 +19,60 @@ export default function useAuth() {
           sendEmailVerification(userCredential.user);
 
           // Add the user to firestore
-          axios.post('/create', {})
+          // axios.post('http://localhost:8080/createStudent', {
+            // name: "a",
+            // mail:"n",
+            // password: "df",
+            // role:"Student",
+            // address: "afasfasf",
+            // phoneNumber: "fafsasf",
+            // hesCode: "asfasfasfa",
+            // ID: 123548,
+            // resideInDorm: true
 
+          //   }
+          // ).then((res) => {
+          //   console.log(res)
+          // })
+          class User {
+            constructor(name, mail, password, role, address, phoneNum) {
+              this.name = name;
+              this.mail = mail;
+              this.password = password;
+              this.role = role;
+              this.address = address;
+              this.phoneNum = phoneNum;
+              this.profilePic = null;
+              this.vaccineCard = null;
+              this.Uid = null;
+              this.orders = null;
+              this.campusStatus = "CAMPUS_ALLOWED";
+            }
+          }
+
+          const x = new User("a", "n", "df", "Student", "wqfqsfafs", "fasfasfasf")
+
+          // axios({
+          //   method: 'post',
+          //   url: 'http://localhost:8080/create',
+          //   data: {
+          //       user: x
+          //   }
+          //   }).then((res) => {
+          //   console.log(res)
+          // })
+
+          axios.post('http://localhost:8080/create', {
+            name: "Ahmed",
+            role: "Student"
+          }, { headers: { 'Content-Type': 'application/json' } })
+
+          // axios.get("http://localhost:8080/abc", { params: {
+          //   a: 5,
+          //   b: 9
+          // }}).then((res) => {
+          //   console.log(res)
+          // })
 
           router.push('/auth/login');
         }
