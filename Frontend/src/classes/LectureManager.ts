f85bbace25ec1c4c9743ff167642714e1742f2d7
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2021-12-25 00:18:30 +0300

    Lecture mechanisms parts 2

--- Frontend/src/classes/LectureManager.ts
+++ Frontend/src/classes/LectureManager.ts
@@ -5,6 +5,7 @@ import Instructor from "./Instructor";
 import Lecture from "./Lecture";
 import {getDatabase, ref, push, set, get, query, orderByChild, equalTo} from "firebase/database";
 import {getAuth} from "firebase/auth";
+import { Loading } from "quasar";
 
 
 export default class LectureManager {
@@ -23,7 +24,7 @@ export default class LectureManager {
     return this.instance;
   }
 
-
+  //Create courses with given parameters
   public async createCourse(lectureName: string, section:number, building:string, place: string) {
     const auth = getAuth();
     const Uid = auth.currentUser?.uid;
@@ -32,9 +33,8 @@ export default class LectureManager {
     let instructor = (await get(ref(db, `Users/${Uid}`))).val();
 
     if(instructor){
-      let ncourseCode = (Math.random() * 10000) + 1;
-      let courseCode = ncourseCode.toString();
-      let LID = (Math.random() * 10000) + 1;
+      let courseCode = (instructor._name.replace(/ /g, "").substring(0,5) + building + section).toLowerCase();
+      let LID = Date.now();
       
       let lecture = new Lecture(instructor._name, lectureName, section, place, courseCode, -1, LID);
 
@@ -43,7 +43,7 @@ export default class LectureManager {
     }
   }
 
-
+  //Generates random lecture code which shown at start of every lecture
   public async generateLectureCode(LID:number){ 
     const db = getDatabase();
 
@@ -54,17 +54,16 @@ export default class LectureManager {
     set(ref(db, `/Lectures/_${lecture.LID}/_${lecture.lectureCode}`),lectureCode);
   }
 
-
-  public async changeSeatStatus(phoneNumber: string) {
-    const auth = getAuth();
-    const Uid = auth.currentUser?.uid;
+  //This function is used to change status of a particular seat from teacher
+  public async changeSeatStatus(LID: number, AID: number, SID: number, confirm: boolean) {
     const db = getDatabase();
-
-    let instructor = (await get(ref(db, `UserGetByUID/${Uid}`))).val(); 
-
+    
+    let attendance = (await get(ref(db,`Lectures/${LID}/Attendances/${AID}`))).val(); // Get attendance object to reach seat plan in it
+    let seat = (await get(ref(db,`Lectures/${LID}/Attendances/${AID}/${attendance._particularLectureSeatPlan}/${SID}`))).val(); // Get particular seat 
+    await set(ref(db,`Lectures/${LID}/SeatPlan/${SID}/${seat._confirm}`), confirm); // Change particular seat's confirm property
   }
 
-   
+  // This function is used to enroll student to a course
   public async enrollStudentToCourse(courseCode: string){
     const auth = getAuth();
     const Uid = auth.currentUser?.uid;
@@ -74,21 +73,26 @@ export default class LectureManager {
     let query1 = query(ref(db, `Lectures`),orderByChild('courseCode'),equalTo(courseCode));
     let lecture = (await get(query1.ref)).val();
 
-    set(ref(db, `Users/${student.Uid}/Lectures/${lecture.LID}/${lecture.lectureCode}`),lecture.lectureCode); 
-    set(ref(db, `Lectures/${lecture.LID}/${lecture.lectureCode}`),lecture.lectureCode);
-    return true;
+    await set(ref(db, `Users/${student.Uid}/Lectures/${lecture.LID}/${lecture.lectureCode}`),lecture.lectureCode); 
+    await set(ref(db, `Lectures/${lecture.LID}/${lecture.lectureCode}`),lecture.lectureCode);
   }
 
 
+  // This function is used to set seat's first owner
   public async setSeatOwner(UID: number, LID: number, SID: number){
-    const auth = getAuth();
-    const Uid = auth.currentUser?.uid;
     const db = getDatabase();
 
-    let student = (await get(ref(db, `Users/${UID}`))).val(); //Get current student
-    let lecture = (await get(ref(db, `Lectures/${LID}`))).val(); //Get current lecture
+    let seat = (await get(ref(db,`Lectures/${LID}/SeatPlan/${SID}`))).val(); 
+    await set(ref(db,`Lectures/${LID}/SeatPlan/${SID}/${seat._studentOwnerUID}`),UID); 
+  }
 
-    // let query1 = query(ref(db, `Lectures/${LID}/SeatPlan/${lecture.}`)
+  // This function get instructor's lectures and returns it in lecture array.
+  public async getInstructorlectures(UID:number,lecture: Lecture[]){
+    const db = getDatabase();
+
+    let instructor = (await get(ref(db, `Users/${UID}`))).val();
+    let query1 = query(ref(db,`Users/${UID}/Lectures`), orderByChild('UID'));
+    lecture = (await get(query1.ref)).val();
 
   }
 }
